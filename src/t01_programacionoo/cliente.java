5a4b4b0b4665d9d66620b74bb3d73a663f8da098
JOTA B <JOTA B@DESKTOP-6ADJG2D>
2021-06-24 18:51:06 -0500

    Deber POO.

--- src/t01_programacionoo/cliente.java
+++ src/t01_programacionoo/cliente.java
@@ -1,5 +1,27 @@
 package t01_programacionoo;
 
 public class cliente {
+/* encapsulacion */
+private String nombre; 
+private String apellido; 
+public String edad; 
 
+/* Abstraccion */
+
+void comprar(String fechainicio, String fechafinal) {
+	int pago; 
+	
+	String precio = fechainicio + fechafinal; 
+	
 }
+
+/* Polimorfismo */
+
+void comprar(String seguro) {
+	int entrada; 
+
+	String nombreseguro; 
+}
+}
+
+/* Herencia */
--- src/t01_programacionoo/vendedor.java
+++ src/t01_programacionoo/vendedor.java
@@ -2,4 +2,14 @@ package t01_programacionoo;
 
 public class vendedor {
 
-}
+	private String nombre; 
+	
+	void vendercliente() {
+		 cliente vender = new cliente(); 
+		 vender.comprar();
+	}
+	
+	void comprar() {
+	}
+	}
+
