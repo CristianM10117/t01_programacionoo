97c8b1ed8a49422da930086b35502e2722f9df4b
JOTA B <JOTA B@DESKTOP-6ADJG2D>
2021-07-07 19:43:06 -0500

    ejercicio 1.

--- src/DiagramasFlujo/MainEjercicio1.java
+++ src/DiagramasFlujo/MainEjercicio1.java
@@ -1,10 +1,24 @@
 package DiagramasFlujo;
-
+import java.util.Scanner; 
 public class MainEjercicio1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+Scanner leer = new Scanner(System.in); 
+		
+		System.out.println("Ingrese el sueldo: ");
+		 
+		 int A = leer.nextInt(); 
+		if (A < 300) {
+			
+		 System.out.println("No recibe bonificacion. ");
+		}else { 
+			int b = A + 40; 
+		 System.out.println("Su bonificacion es: " +b);
+		
+	}
+	
+	
 	}
 
 }
