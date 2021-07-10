f5ccf2aad759f8be4ab7b45c0533369c0f396d47
JOTA B <JOTA B@DESKTOP-6ADJG2D>
2021-07-09 19:41:04 -0500

    Tabla del 7.

---
+++ src/Diagramas/de/Flujo/Main1.java
@@ -0,0 +1,21 @@
+package Diagramas.de.Flujo;
+
+import java.util.Scanner;
+
+public class Main1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/* Imprimir la tabla del 7 multiplicado hasta el 12 */
+		System.out.println("Números impares. ");
+
+		for (int a = 0; a <= 12; a++) {
+
+			int b = 7 * a; 
+			
+			System.out.println("7 * "+a+" = "  +b);
+
+		}
+	}
+
+}
