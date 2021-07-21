e5c1b912140ac765724a6c086735a5b1c8448417
JOTA B <JOTA B@DESKTOP-6ADJG2D>
2021-07-21 18:51:50 -0500

    taller.

---
+++ src/ejemplos/de/tipos/de/datos/MainTiposDeDatos.java
@@ -0,0 +1,19 @@
+package ejemplos.de.tipos.de.datos;
+
+public class MainTiposDeDatos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  short cantidadestudiantes = 1000; 
+  byte entradacircuito = 1; 
+  int cantciudadanos = 10000; 
+  long cantidadmundo = 10000000; 
+  float sueldo = (short) 1000.57;
+  double saldobanco = 10000000.43; 
+  char opcionmenu = 'C';
+  boolean casado = true; 
+	
+	
+	}
+
+}
