357ff905a3fad610ae5dd8d77ae59caa3a9a2cf6
JOTA B <JOTA B@DESKTOP-6ADJG2D>
2021-08-13 15:31:24 -0500

    Deber en pareja.

---
+++ src/deber/en/pareja/FacturaGasolinera.java
@@ -0,0 +1,13 @@
+package deber.en.pareja;
+
+public class FacturaGasolinera {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	Cliente cliente1 = new Cliente(); 
+	cliente1.DatosCliente("Cristian", "Chulca", "1728040336", "GDF - 123", "Diesel", +5);
+	
+	}
+
+}
